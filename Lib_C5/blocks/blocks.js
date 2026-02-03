6430bd32df980c8ca8a73515bc32117aebd05333
xixihahaed <75563352+xiaojieyueyue@users.noreply.github.com>
2026-02-03 19:42:51 +0800

    Delete Lib_C5 directory

--- Lib_C5/blocks/blocks.js
+++
@@ -1,8 +0,0 @@
-Blockly.Blocks['mono_c5_test'] = {
-    init: function () {
-        this.appendDummyInput().appendField("Monorepo C5 Test");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(100);
-    }
-};
--- Lib_C5/generator/generator.js
+++
@@ -1,3 +0,0 @@
-Blockly.Arduino['mono_c5_test'] = function (block) {
-    return 'Serial.println("Hello from Monorepo C5");\n';
-};
