369f16a2fa2586a710677eb7086ef60a04dccfe5
xixihahaed <75563352+xiaojieyueyue@users.noreply.github.com>
2026-02-03 19:43:04 +0800

    Delete Lib_Std directory

--- Lib_Std/blocks/blocks.js
+++
@@ -1,8 +0,0 @@
-Blockly.Blocks['mono_std_test'] = {
-    init: function () {
-        this.appendDummyInput().appendField("Monorepo Std Test");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(200);
-    }
-};
--- Lib_Std/generator/generator.js
+++
@@ -1,3 +0,0 @@
-Blockly.Arduino['mono_std_test'] = function (block) {
-    return 'Serial.println("Hello from Monorepo Std");\n';
-};
