5cf9fd521c4830489115cff3d2add79d05903b0c
xiaojieyueyue <xiaojieyueyue@users.noreply.github.com>
2026-02-15 20:43:10 +0800

    Use ORDER_ATOMIC in test library generators

--- CK_AVR_Blink_Test/blocks/generator.js
+++ CK_AVR_Blink_Test/blocks/generator.js
@@ -6,8 +6,9 @@
 
 Blockly.Arduino['ck_avr_led_blink_once'] = function(block) {
   var pin = block.getFieldValue('PIN') || '13';
-  var ms = Blockly.Arduino.valueToCode(block, 'MS', Blockly.Arduino.ORDER_NONE) || '200';
+  var ms = Blockly.Arduino.valueToCode(block, 'MS', Blockly.Arduino.ORDER_ATOMIC) || '200';
   Blockly.Arduino.definitions_['include_ck_avr_blink_test'] = '#include "CK_AVR_Blink_Test.h"';
   return 'ck_avr_blink_once((uint8_t)' + pin + ', (unsigned long)(' + ms + '));\n';
 };
+
 
