8a4138e19dda2fd7fc3cd5a21fe494040aae651c
xiaojieyueyue <xiaojieyueyue@users.noreply.github.com>
2026-02-15 20:20:49 +0800

    Fix JS quote syntax in test Blockly libraries

--- CK_AVR_Blink_Test/blocks/blocks.js
+++ CK_AVR_Blink_Test/blocks/blocks.js
@@ -1,32 +1,33 @@
-Blockly.Blocks[''ck_avr_led_init''] = {
+﻿Blockly.Blocks['ck_avr_led_init'] = {
   init: function() {
     this.appendDummyInput()
-      .appendField(''AVR LED init pin'')
+      .appendField('AVR LED init pin')
       .appendField(new Blockly.FieldDropdown([
-        [''13'', ''13''],
-        [''12'', ''12''],
-        [''11'', ''11''],
-        [''10'', ''10'']
-      ]), ''PIN'');
+        ['13', '13'],
+        ['12', '12'],
+        ['11', '11'],
+        ['10', '10']
+      ]), 'PIN');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(190);
   }
 };
 
-Blockly.Blocks[''ck_avr_led_blink_once''] = {
+Blockly.Blocks['ck_avr_led_blink_once'] = {
   init: function() {
     this.appendDummyInput()
-      .appendField(''AVR blink pin'')
+      .appendField('AVR blink pin')
       .appendField(new Blockly.FieldDropdown([
-        [''13'', ''13''],
-        [''12'', ''12''],
-        [''11'', ''11''],
-        [''10'', ''10'']
-      ]), ''PIN'');
-    this.appendValueInput(''MS'').setCheck(''Number'').appendField(''delay ms'');
+        ['13', '13'],
+        ['12', '12'],
+        ['11', '11'],
+        ['10', '10']
+      ]), 'PIN');
+    this.appendValueInput('MS').setCheck('Number').appendField('delay ms');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(190);
   }
 };
+
--- CK_ESP32C5_WiFi_Test/blocks/blocks.js
+++ CK_ESP32C5_WiFi_Test/blocks/blocks.js
@@ -1,25 +1,25 @@
-Blockly.Blocks[''ck_esp32c5_wifi_connect''] = {
+Blockly.Blocks['ck_esp32c5_wifi_connect'] = {
   init: function() {
-    this.appendValueInput(''SSID'').setCheck(null).appendField(''ESP32 WiFi SSID'');
-    this.appendValueInput(''PASS'').setCheck(null).appendField(''Password'');
+    this.appendValueInput('SSID').setCheck(null).appendField('ESP32 WiFi SSID');
+    this.appendValueInput('PASS').setCheck(null).appendField('Password');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(215);
   }
 };
 
-Blockly.Blocks[''ck_esp32c5_wifi_connected''] = {
+Blockly.Blocks['ck_esp32c5_wifi_connected'] = {
   init: function() {
-    this.appendDummyInput().appendField(''ESP32 WiFi connected?'');
-    this.setOutput(true, ''Boolean'');
+    this.appendDummyInput().appendField('ESP32 WiFi connected?');
+    this.setOutput(true, 'Boolean');
     this.setColour(215);
   }
 };
 
-Blockly.Blocks[''ck_esp32c5_wifi_ip''] = {
+Blockly.Blocks['ck_esp32c5_wifi_ip'] = {
   init: function() {
-    this.appendDummyInput().appendField(''ESP32 local IP'');
-    this.setOutput(true, ''String'');
+    this.appendDummyInput().appendField('ESP32 local IP');
+    this.setOutput(true, 'String');
     this.setColour(215);
   }
 };
